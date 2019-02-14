02259bcfcfc8b7f01e1422895e550be8e6021cfa
Alan Reis Belarmino <alanreisb@gmail.com>
2019-02-14 10:34:45 -0200

    Updates

---
+++ precache-manifest.14ce6a8b3e2460d7748a936f717f6f5d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/Portfolio-AlanReis/static/media/jquery.a3af0655.png"
+  },
+  {
+    "revision": "1e92400b0335866f9530",
+    "url": "/Portfolio-AlanReis/static/css/main.d5c51878.chunk.css"
+  },
+  {
+    "revision": "580fb2911444ee60d516546ddd001ba7",
+    "url": "/Portfolio-AlanReis/static/media/objetivo.580fb291.svg"
+  },
+  {
+    "revision": "de973b4a53e1e079a630",
+    "url": "/Portfolio-AlanReis/static/js/1.de973b4a.chunk.js"
+  },
+  {
+    "revision": "41626a3d0c59e23ee6f3",
+    "url": "/Portfolio-AlanReis/static/js/runtime~main.41626a3d.js"
+  },
+  {
+    "revision": "aa088fead6687df8da1b1001171c8eca",
+    "url": "/Portfolio-AlanReis/static/media/seta.aa088fea.svg"
+  },
+  {
+    "revision": "dd8829a52b58730ccb18a138b3d92bfa",
+    "url": "/Portfolio-AlanReis/static/media/bgWave.dd8829a5.svg"
+  },
+  {
+    "revision": "e640a867da3d12ba4810e19a8fa6144e",
+    "url": "/Portfolio-AlanReis/static/media/github.e640a867.svg"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/Portfolio-AlanReis/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "1e92400b0335866f9530",
+    "url": "/Portfolio-AlanReis/static/js/main.1e92400b.chunk.js"
+  },
+  {
+    "revision": "826879c3383772b241b3c337b8da04eb",
+    "url": "/Portfolio-AlanReis/static/media/csharp.826879c3.png"
+  },
+  {
+    "revision": "994977c48fde58ac674a2d05ba5a5efb",
+    "url": "/Portfolio-AlanReis/static/media/nodejs.994977c4.png"
+  },
+  {
+    "revision": "30f627a1db56e7a9b96729d516f82879",
+    "url": "/Portfolio-AlanReis/static/media/FotoPerfil.30f627a1.jpg"
+  },
+  {
+    "revision": "b2ce74a5289aae5fa74d4956318b1e4a",
+    "url": "/Portfolio-AlanReis/static/media/BackgroundGrande.b2ce74a5.jpg"
+  },
+  {
+    "revision": "668d86a2fb8d5145a821bcba28b61963",
+    "url": "/Portfolio-AlanReis/static/media/apresentacao.668d86a2.svg"
+  },
+  {
+    "revision": "af41dd5f750983da2a8c79af7c5d4970",
+    "url": "/Portfolio-AlanReis/static/media/contatos.af41dd5f.svg"
+  },
+  {
+    "revision": "ca9b18f4f85dd8aebaa3004951361a84",
+    "url": "/Portfolio-AlanReis/static/media/habilidades.ca9b18f4.svg"
+  },
+  {
+    "revision": "de973b4a53e1e079a630",
+    "url": "/Portfolio-AlanReis/static/css/1.d3ca4d5a.chunk.css"
+  },
+  {
+    "revision": "618456202e8849e65cf8b9eb02d3eeb7",
+    "url": "/Portfolio-AlanReis/index.html"
+  }
+];
