3236290c44b75b762223a3e05a2194ca0933c061
Alan Reis Belarmino <alanreisb@gmail.com>
2019-02-15 19:20:50 -0200

    Updates

--- precache-manifest.a0a421fea53ec7eac857a43d57217489.js
+++ precache-manifest.a0a421fea53ec7eac857a43d57217489.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "a3af0655fc9a481401550b5be0e86cab",
-    "url": "/Portfolio-AlanReis/static/media/jquery.a3af0655.png"
+    "revision": "826879c3383772b241b3c337b8da04eb",
+    "url": "/Portfolio-AlanReis/static/media/csharp.826879c3.png"
   },
   {
-    "revision": "e877bc199c63445db031",
-    "url": "/Portfolio-AlanReis/static/css/main.d5c51878.chunk.css"
+    "revision": "effbe6310518f36be722",
+    "url": "/Portfolio-AlanReis/static/css/main.b0d48e2f.chunk.css"
   },
   {
     "revision": "580fb2911444ee60d516546ddd001ba7",
     "url": "/Portfolio-AlanReis/static/media/objetivo.580fb291.svg"
   },
   {
-    "revision": "6baa92418fd679dd1c3c",
-    "url": "/Portfolio-AlanReis/static/js/1.6baa9241.chunk.js"
+    "revision": "67595978c52526322da4",
+    "url": "/Portfolio-AlanReis/static/js/1.67595978.chunk.js"
   },
   {
     "revision": "41626a3d0c59e23ee6f3",
@@ -28,20 +28,16 @@ self.__precacheManifest = [
     "url": "/Portfolio-AlanReis/static/media/bgWave.dd8829a5.svg"
   },
   {
-    "revision": "e640a867da3d12ba4810e19a8fa6144e",
-    "url": "/Portfolio-AlanReis/static/media/github.e640a867.svg"
-  },
-  {
     "revision": "d14e4fd3c8b856f05612643d6773e497",
     "url": "/Portfolio-AlanReis/static/media/react.d14e4fd3.png"
   },
   {
-    "revision": "e877bc199c63445db031",
-    "url": "/Portfolio-AlanReis/static/js/main.e877bc19.chunk.js"
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/Portfolio-AlanReis/static/media/jquery.a3af0655.png"
   },
   {
-    "revision": "826879c3383772b241b3c337b8da04eb",
-    "url": "/Portfolio-AlanReis/static/media/csharp.826879c3.png"
+    "revision": "effbe6310518f36be722",
+    "url": "/Portfolio-AlanReis/static/js/main.effbe631.chunk.js"
   },
   {
     "revision": "994977c48fde58ac674a2d05ba5a5efb",
@@ -50,6 +46,10 @@ self.__precacheManifest = [
   {
     "revision": "30f627a1db56e7a9b96729d516f82879",
     "url": "/Portfolio-AlanReis/static/media/FotoPerfil.30f627a1.jpg"
+  },
+  {
+    "revision": "e640a867da3d12ba4810e19a8fa6144e",
+    "url": "/Portfolio-AlanReis/static/media/github.e640a867.svg"
   },
   {
     "revision": "b2ce74a5289aae5fa74d4956318b1e4a",
@@ -68,11 +68,11 @@ self.__precacheManifest = [
     "url": "/Portfolio-AlanReis/static/media/habilidades.ca9b18f4.svg"
   },
   {
-    "revision": "6baa92418fd679dd1c3c",
-    "url": "/Portfolio-AlanReis/static/css/1.d3ca4d5a.chunk.css"
+    "revision": "67595978c52526322da4",
+    "url": "/Portfolio-AlanReis/static/css/1.de930a5e.chunk.css"
   },
   {
-    "revision": "e143e0972cc75ff4fc21a982fc3eee0b",
+    "revision": "c3bbadb9c007340a539f801b445b7d06",
     "url": "/Portfolio-AlanReis/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-AlanReis/precache-manifest.4c869382eaea6df4f208b0182407813a.js"
+  "/Portfolio-AlanReis/precache-manifest.a0a421fea53ec7eac857a43d57217489.js"
 );
 
 workbox.clientsClaim();
